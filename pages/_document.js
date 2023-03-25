e8ba574e89834c06a248318d546026ce3aeff09f
Tufan Gökmenler <tfngkmnlr@gmail.com>
2023-03-25 03:20:57 +0300

    Add developer information

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <div className={"position-fixed start-50 translate-middle-x bottom-0 p-3"}>
+            <p className={"mb-0 text-white"}>Developed by <a href="https://www.tufangokmenler.com" className={"text-warning text-decoration-none"} target="_blank">Tufan Gökmenler</a> | Visit my <a href="https://github.com/tufkan1/" target={"_blank"} className={"text-warning text-decoration-none"}>Github</a></p>
+        </div>
       </body>
     </Html>
   )
